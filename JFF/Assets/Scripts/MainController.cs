bebad235a0fb7d5b4bb94909eadf7b872782c475
Nikonova Nataliya <natasha.nickonova@gmail.com>
2016-06-11 22:20:52 +0300

    version v00.00.01

---
+++ JFF/Assets/Scripts/MainController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MainController : MonoBehaviour {
+
+	public Vector3 m_startPosition;
+
+	// Use this for initialization
+	void Start () {
+		GameObject centerRotation = GameObject.CreatePrimitive( PrimitiveType.Cube );
+		Transform centerRotationTransform = centerRotation.GetComponent<Transform>();
+		centerRotationTransform.localPosition = m_startPosition;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		// int a = 5;
+		// float c = 6.0f;
+		// if( typeOf( a ) == typeOf( float ) ) {
+
+		// }
+
+		float currentTime = Time.realtimeSinceStartup;
+
+		GameObject myGameObject = this.gameObject;
+		Transform myTransform = myGameObject.GetComponent<Transform>();
+		Vector3 currentPosition = new Vector3( 0, 0, 0 );
+		currentPosition.x = Mathf.Sin( currentTime );
+		currentPosition.y = Mathf.Cos( currentTime );
+		currentPosition.z = 0.0f;
+		currentPosition = currentPosition + m_startPosition;
+		myTransform.localPosition = currentPosition;
+	}
+}
+// struct Vector3 {
+// 	public float x;
+// 	public float y;
+// 	public float z;
+// }
