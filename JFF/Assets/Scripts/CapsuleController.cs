3baf1d26bde18ec3bbb205064b06809625689bbf
Nikonova Nataliya <natasha.nickonova@gmail.com>
2016-06-27 12:09:05 +0300

    first_mix

---
+++ JFF/Assets/Scripts/CapsuleController.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CapsuleController : MonoBehaviour {
+
+	
+	// Update is called once per frame
+	void Update () {
+	
+	transform.Rotate(new Vector3(0,0,30)*Time.deltaTime);
+	}
+}
