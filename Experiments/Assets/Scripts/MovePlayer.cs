e51e121610d9bd91d231dd0871416d9a13421fe4
Nikonova Nataliya <natasha.nickonova@gmail.com>
2016-07-02 23:36:02 +0300

    Исправлены движения корабля.

--- Experiments/Assets/Scripts/MovePlayer.cs
+++ Experiments/Assets/Scripts/MovePlayer.cs
@@ -5,7 +5,7 @@ public class MovePlayer : MonoBehaviour {
 
     public float stopStart = 1.5f, speed = 5f, rotationSpees = 100f, heightPlayer = 1f;
 
-    private float mag, angleToTarget;
+    private float mag;
     private Ray ray;
     private RaycastHit hit;
     private Vector3 dir;
@@ -23,45 +23,63 @@ public class MovePlayer : MonoBehaviour {
                 target = hit.point;
             }
         }
-        LookAtThis();
-        MoveTo();        
+        float possibleRotation = MoveTo();
+        LookAtThis( 1.0f );
 	}
 
-    private void CalculateAngle(Vector3 temp) {
+    private float CalculateAngle(Vector3 temp) {
 
         dir = new Vector3(temp.x, transform.position.y, temp.z) - transform.position;
-        angleToTarget = Vector3.Angle(dir, transform.forward);
+        return Vector3.Angle(dir, transform.forward);
 
     }
 
-    private void LookAtThis()
+    private void LookAtThis( float possibleRotation )
     {
         if (target != lastTarget)
         {
 
-            CalculateAngle(target);
+            float angleToTarget = CalculateAngle(target);
+            Debug.Log("angleToTarget : " + angleToTarget);
             if (angleToTarget > 3)
+            {
                 transform.rotation = Quaternion.RotateTowards(transform.rotation,
-                    Quaternion.LookRotation(dir), rotationSpees * Time.deltaTime);
+                    Quaternion.LookRotation(dir), possibleRotation * rotationSpees * Time.deltaTime);
+
+            }
 
         }
     }
 
-    private void MoveTo() {
+    private float MoveTo() {
 
-        if (target != lastTarget) { 
-        
-            mag = (transform.position - target).magnitude;
-            transform.position = Vector3.MoveTowards(transform.position, target, mag >
-                stopStart ? speed * UnityEngine.Time.deltaTime : Mathf.Lerp(speed * 0.5f, speed, mag / stopStart)
-                * UnityEngine.Time.deltaTime);
-            ray = new Ray(transform.position, -Vector3.up);
-            if (Physics.Raycast(ray, out hit, 1000.0f)) { 
-            
-                transform.position = new Vector3(transform.position.x, hit.point.y + heightPlayer, transform.position.z);
-            }
-        }
-        else
+        if (target == lastTarget)
+        {
             lastTarget = target;
+            return 0;
+        }
+        mag = (transform.position - target).magnitude;
+        Vector3 oldPosition = transform.position;
+        Vector3 mathPosition = Vector3.MoveTowards(transform.position, target, mag >
+            stopStart ? speed * UnityEngine.Time.deltaTime : Mathf.Lerp(speed * 0.5f, speed, mag / stopStart)
+            * UnityEngine.Time.deltaTime);
+
+        Vector3 deltaPosition = mathPosition - oldPosition;
+        Vector3 projectDeltaPosition = Vector3.Project(deltaPosition, transform.forward);
+
+        Vector3 projectDeltaPositionInLocal = transform.InverseTransformDirection(projectDeltaPosition);
+        if( projectDeltaPositionInLocal.z < 0 )
+        {
+            return 0;
+        }
+
+        transform.position = oldPosition + projectDeltaPosition;
+
+        ray = new Ray(transform.position, Vector3.up * (-1) );
+        if (Physics.Raycast(ray, out hit, 1000.0f))
+        {
+            transform.position = new Vector3(transform.position.x, hit.point.y + heightPlayer, transform.position.z);
+        }
+        return Vector3.Distance( oldPosition, transform.position );
     }
 }
