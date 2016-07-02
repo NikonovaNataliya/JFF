1f81d51ce1062624c8acd06cb27eb9f67d8dd699
Nikonova Nataliya <natasha.nickonova@gmail.com>
2016-07-02 20:11:51 +0300

    Experiments_move

---
+++ Experiments/Assets/Scripts/Follow.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Follow : MonoBehaviour {
+
+    // public Transform myTransform;
+    public GameObject player;
+    private Vector3 offset;
+
+    void Start()
+    {
+        // Transform cam = GetComponent<Transform>();
+        //        Debug.Log(cam);
+        offset = transform.position - player.transform.position;
+    }
+
+	//void Update () {
+
+ //       transform.LookAt(myTransform);
+	//}
+    void LateUpdate()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
---
+++ Experiments/Assets/Scripts/TriggerOnTarget.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerOnTarget : MonoBehaviour {
+
+    public ShipController player;
+
+    void OnTriggerEnter(Collider onPlace) { 
+    
+        player.OnPlaceTrue();
+    }
+}
