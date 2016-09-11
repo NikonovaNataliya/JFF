7a26343ed47a2148a4ef5f24df8482d132744b8f
Chifffa <d_nik92@mail.ru>
2016-09-11 21:03:39 +0300

    Fixed moves of player

--- Sea Battle/Assets/Scripts/CubeMoveClick.cs
+++ Sea Battle/Assets/Scripts/CubeMoveClick.cs
@@ -14,36 +14,47 @@ public class CubeMoveClick : MonoBehaviour {
     Vector3 lastTarget = new Vector3();
     Vector3 dir;
 
+    private Quaternion m_currentTargetRotation = Quaternion.identity;
+    private float m_closeEnough = 0.1f;
+    private Plane myPlane;
 	void Start () {
         controller = GetComponent<CharacterController>();
         anim = GetComponent<Animator>();
+       this.myPlane = new Plane( Vector3.up, Vector3.zero );
 	}	
 
 	void Update () {
 
         if (Input.GetMouseButtonDown(1)) {
             ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
-            if (Physics.Raycast(ray, out hit, 1000.0f)) {
-                target = hit.point;
+            
+            float distance;
+            if( this.myPlane.Raycast( ray, out distance ) ) {
+                target = ray.GetPoint( distance );
             }
+            // if (Physics.Raycast(ray, out hit, 1000.0f)) {
+            //     target = hit.point;
+            // }
 
             dir = target - transform.position;
             dir = new Vector3(dir.x, 0, dir.z);
             dir.Normalize();
 
-            Quaternion look = Quaternion.LookRotation(dir);
-            transform.rotation = Quaternion.Slerp(transform.rotation, look, Time.deltaTime * rotationSpeed);
+           m_currentTargetRotation = Quaternion.LookRotation(dir);
 
+        }
+        float angle = Quaternion.Angle(transform.rotation, m_currentTargetRotation);
+        if( Mathf.Abs( angle ) > 0.1f ) {
+            transform.rotation = Quaternion.Slerp(transform.rotation, m_currentTargetRotation, Time.deltaTime * rotationSpeed);
         }
         dir.y -= gravity * Time.deltaTime;
 
-        float target_x =(int)target.x;
-        float lastTarget_x = (int)lastTarget.x;
-        float target_z = (int)target.z;
-        float lastTarget_z = (int)lastTarget.z;
+        // float target_x =(int)target.x;
+        // float lastTarget_x = (int)lastTarget.x;
+        // float target_z = (int)target.z;
+        // float lastTarget_z = (int)lastTarget.z;
 
-        if ((target_x != lastTarget_x) || (target_z != lastTarget_z)) {
+        if( Vector3.Distance( target, lastTarget ) > m_closeEnough ) { // }   (target_x != lastTarget_x) || (target_z != lastTarget_z)) {
             controller.Move(dir * Time.deltaTime * moveSpeed);
             lastTarget = new Vector3(transform.position.x, 0, transform.position.z);
             anim.SetBool("Idle-Walk", true);
